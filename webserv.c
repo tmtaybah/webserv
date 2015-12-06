7dc41520e6c2bc5ebf24608ea5c2ed6f8db0e18b
tmtaybah <tara.taybah@gmail.com>
2015-12-05 22:53:55 -0500

    port section - no testing

--- webserv.c
+++ webserv.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 
-//==============================================================================
-//====== Main
-//==============================================================================
+//============================= Main ==================================
 
 int main (int argc, char **argv)
 {
+
+  if (argc > 1){      // check if port num is between 5000-65536
+    int port_num = strtoimax(argv[1], NULL, 10);
+
+    if(port_num < 5000 || port_num > 65536){
+        fprintf(stderr, "Port number must between 5000-65536\n");
+        return -1;
+    }
+
+  }
+  else{
+    fprintf(stderr, "Must insert port number between 5000-65536\n");
+    return -1;
+  }
+
+
 }
